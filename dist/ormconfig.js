2e0974176069de2933b839969f5e577f43b2ed3a
Adriana Lima <adrianalimafm@gmail.com>
2020-08-14 10:22:14 -0300

    Path config

---
+++ dist/ormconfig.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("process.env.HEROKU_POSTGRESQL_GRAY_UR :>>", process.env.HEROKU_POSTGRESQL_GRAY_UR);
+module.exports = {
+    type: "postgres",
+    url: process.env.HEROKU_POSTGRESQL_GRAY_URL,
+    entities: ["dist/models/*.js"],
+    migrations: ["dist/database/migrations/*.js"],
+    cli: {
+        migrationsDir: "./src/database/migrations",
+    },
+};
--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,12 @@
+console.log(
+  "process.env.HEROKU_POSTGRESQL_GRAY_UR :>>",
+  process.env.HEROKU_POSTGRESQL_GRAY_UR
+);
 module.exports = {
   type: "postgres",
   url: process.env.HEROKU_POSTGRESQL_GRAY_URL,
-  entities: [".dist/models/*.js"],
-  migrations: ["dist/database/migrations/*.js"],
+  entities: ["dist/src/models/*.js"],
+  migrations: ["dist/src/database/migrations/*.js"],
   cli: {
     migrationsDir: "./src/database/migrations",
   },
