c39023d751c682c7695c84a693698cdf6a8f51cf
dxwebster <adrianalimafm@gmail.com>
2020-08-21 16:27:50 -0300

    Config envs

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  "url": process.env.TYPEORM_URL,
+
+  "entities": process.env.TYPEORM_ENTITIES,
+  "migrations": process.env.TYPEORM_MIGRATIONS,
+  "cli": {
+    "migrationsDir": "./src/database/migrations"
+  }
+} 
