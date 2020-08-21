b896e144b44b8f10c5d9f0b066e1dd3e6d9d75b7
dxwebster <adrianalimafm@gmail.com>
2020-08-21 10:32:05 -0300

    Updates

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,11 +6,10 @@ export default async (name = "default"): Promise<Connection> => {
   return createConnection(
     Object.assign(defaultOptions, {
       name,
-      database: "postgres://cbwvxjnhhgmtbd:cca9c4fad51f1933b9c70be6112e100c6e5d2461c0d80f289afa15918d16e403@ec2-52-1-95-247.compute-1.amazonaws.com:5432/damc4veaguffd8"
-      // database:
-      //   process.env.NODE_ENV === "test"
-      //     ? "gostack_desafio06_tests"
-      //     : defaultOptions.database,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "gostack_desafio06_tests"
+          : defaultOptions.database,
     })
   );
 };
