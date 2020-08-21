8ab7223bf69da869f878f7f11cb5a5f7806713ab
dxwebster <adrianalimafm@gmail.com>
2020-08-21 18:04:34 -0300

    config var env

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,10 @@
 module.exports = {
   "type": postgres,
   "url": process.env.DATABASE_URL,
-  "entities": process.env.TYPEORM_ENTITIES,
-  "migrations": process.env.TYPEORM_MIGRATIONS,
+  
+  "entities": "./dist/models/*.js",
+  "migrations": "./dist/database/migrations/*.js",
+  
   "cli": {
     "migrationsDir": "./src/database/migrations"
   }
